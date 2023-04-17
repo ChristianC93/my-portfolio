220fac3cb9d5117f818b1b4ac3c5194651c8f1c9
Christian Castillo <castillochristian3@gmail.com>
2023-04-17 16:37:14 -0400

    created routes for project, about, contact components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Home from "./components/Home";
 import { Routes, Route } from "react-router-dom";
+import Project from "./components/Project";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={ <Home /> } />
+        <Route path="/project" element={ <Project /> } />
+        <Route path="/about" element={ <About /> } />
+        <Route path="/contact" element={ <Contact /> } />
       </Routes>
     </div>
   );
