0c0f46b556becf71bd47ec364eb3d27e569974fe
Christian Castillo <castillochristian3@gmail.com>
2023-04-17 12:56:33 -0400

    initialized home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
+      <h1>This is app.js</h1>
+      <Home />
     </div>
   );
 }
