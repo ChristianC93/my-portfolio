f859b53a758dd338ba099dd3b34cf62cdec91462
Christian Castillo <castillochristian3@gmail.com>
2023-04-17 17:47:34 -0400

    updated NavBar styling, conditional rendering facomponents

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
-import "./NavBarStyle.css"
-import React from 'react'
+import { FaBars, FaTimes} from "react-icons/fa";
+import "./NavBarStyle.css";
+import React, { useState } from 'react';
 
 
 const NavBar = () => {
+    const [hidden, setHidden] = useState(false);
+
+    const handleClick = () => {
+        setHidden(!hidden);
+    }
+
   return (
     <div className="header">
         <Link to="/">
@@ -11,7 +18,7 @@ const NavBar = () => {
         </Link>
         <ul className="nav-menu">
             <li>
-                <Link to="/home">Home</Link>
+                <Link to="/">Home</Link>
             </li>
             <li>
                 <Link to="/project">Project</Link>
@@ -23,6 +30,13 @@ const NavBar = () => {
                 <Link to="/contact">Contact</Link>
             </li>
         </ul>
+        <div className="hamburger" onClick={ handleClick }>
+            {hidden ? (
+                <FaTimes size={ 20 } style={{ color: "white" }} />
+            ) : (
+                <FaBars size={ 20 } style={{ color: "white" }} />
+            ) }     
+        </div>
     </div>
   );
 }
