43ee1147ff7dc363b160e79148c11b65785f2b29
Christian Castillo <castillochristian3@gmail.com>
2023-04-20 11:28:21 -0400

    created AboutContent component and stylesheet

---
+++ src/components/AboutContent.js
@@ -0,0 +1,10 @@
+import '../styles/AboutContentStyle.css'
+import React from 'react'
+
+const AboutContent = () => {
+  return (
+    <div>AboutContent</div>
+  );
+}
+
+export default AboutContent;
--- src/routes/About.js
+++ src/routes/About.js
@@ -2,12 +2,14 @@ import React from 'react'
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import BackGround2 from '../components/BackGround2';
+import AboutContent from '../components/AboutContent';
 
 const About = () => {
   return (
     <div>
       <NavBar />
       <BackGround2 heading="About Me" text="A software delevoper in New York" />
+      <AboutContent />
       <Footer />
     </div>
   );
