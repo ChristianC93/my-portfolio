82575befce7bc48475fd73d0c1b924b6bee2e3d2
Christian Castillo <castillochristian3@gmail.com>
2023-04-20 12:27:06 -0400

    updated aboutcontent and style

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,9 +1,32 @@
+import { Link } from 'react-router-dom';
 import '../styles/AboutContentStyle.css'
 import React from 'react'
+import JS from '../assets/Js.webp';
+import rails from '../assets/rails.png';
+
+
 
 const AboutContent = () => {
   return (
-    <div>AboutContent</div>
+    <div className='about'>
+        <div className='left'>
+            <h1>Who Am I?</h1>
+            <p>I am a software delevoper based in New York City. My skills include JavaScript, React, Redux, CSS, Ruby on Rails, SQL, and Git</p>
+            <Link to='/contact'>
+                <button className='btn'>Contact</button>
+            </Link>
+        </div>
+        <div className='right'>
+            <div className='img-container'>
+                <div className='img-stack top'>
+                    <img src={ JS } className='img' alt='jsimg'/>
+                </div>
+                <div className='img-stack bottom'>
+                    <img src={ rails } className='img' alt='railsimg'/>
+                </div>
+            </div>
+        </div>
+    </div>
   );
 }
 
